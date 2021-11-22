541648388c64b7439c1e0a90fbba761042f0786e
unknown <n.rahnama1996@gmail.com>
2021-11-22 19:05:14 +0330

    first

--- list/src/components/Users/AddUser.js
+++ list/src/components/Users/AddUser.js
@@ -22,7 +22,7 @@ return;
 if (+enteredAge<1){
     return;
 }
-        console.log(enteredUsername , enteredAge);
+        props.onAddUser(enteredUsername,enteredAge);
         setEnteredUsername('');
         setEnteredAge('');
     }
